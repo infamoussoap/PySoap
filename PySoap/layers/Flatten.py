fixed - flatten layer had a bug where it wasnt properly passing the gradients through

PySoap/layers/Flatten.py
@@ -48,7 +48,7 @@ class Flatten(Layer):
 
         self.built = True
 
-    def predict(self, z, output_only=True):
+    def predict(self, z, output_only=True, pre_activation_of_input=None):
         """ Returns the prediction of this layer
 
             Parameters
@@ -59,6 +59,8 @@ class Flatten(Layer):
                 If set to true, then this function will return only the prediction of the neural
                 network. If set to false then this will return the outputs of the individual
                 layers. Unless back propagation is being performed, this should be set to true.
+            pre_activation_of_input : (N, *input_shape) np.array
+                The input, z, before it passed through the activation function
 
             Returns
             -------
@@ -80,7 +82,7 @@ class Flatten(Layer):
 
         if output_only:
             return z.reshape(len(z), self.output_shape[0])
-        return z, z.reshape(len(z), self.output_shape[0])
+        return pre_activation_of_input, z.reshape(len(z), self.output_shape[0])
 
     def get_delta_backprop_(self, g_prime, new_delta, *args, **kwargs):
         """ Returns the delta for the previous layer, delta^{k-1}_{m,j}.